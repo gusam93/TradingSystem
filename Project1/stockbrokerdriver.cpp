10d9b04217f935e365b4cb7bcfcf9fbe50d06238
SOOEON <bobstar0@naver.com>
2025-07-04 10:39:34 +0900

    delete std

--- Project1/stockbrokerdriver.cpp
+++ Project1/stockbrokerdriver.cpp
@@ -8,14 +8,14 @@ using std::string;
 class StockBrockerDriver {
 public:
 	virtual void login(string id, string password) = 0;
-	virtual void sell(std::string stockCode, int price, int count) = 0;
+	virtual void sell(string stockCode, int price, int count) = 0;
 };
 
 class KiwerDriver : public StockBrockerDriver {
 	void login(string id, string password) override {
 		return kiwerapi->login(id, password);
 	}
-	void sell(std::string stockCode, int price, int count) {
+	void sell(string stockCode, int price, int count) {
 		return kiwerapi->sell(stockCode, count, price);
 	}
 private:
@@ -26,7 +26,7 @@ class NemoDriver : public StockBrockerDriver {
 	void login(string id, string password) override {
 		return nemoapi->certification(id, password);
 	}
-	void sell(std::string stockCode, int price, int count) {
+	void sell(string stockCode, int price, int count) {
 		return nemoapi->sellingStock(stockCode, price, count);
 	}
 private:
